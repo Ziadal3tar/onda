15ca6745a2a852369397396d80eab8799ac96885
Ziadal3tar <ziadalattar22@gmail.com>
2023-12-23 23:58:46 +0200

    '7ed'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BookingComponent } from './components/booking/booking.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 const routes: Routes = [
   {path:'',redirectTo:"home",pathMatch:'full'},
   {path: 'home', component:HomeComponent},
   {path: 'Tickets/:id', component:BookingComponent},
+  {path: 'AboutUs', component:AboutusComponent},
 ];
 
 @NgModule({
